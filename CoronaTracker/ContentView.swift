daf1c8c84152e4a28ee8388b4c954a0c0adf816b
Catalin Olaru <catalinlr15@gmail.com>
2020-04-06 02:47:46 +0300

    Initial Commit

---
+++ CoronaTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoronaTracker
+//
+//  Created by Catalin Olaru on 4/6/20.
+//  Copyright © 2020 Catalin Olaru. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
